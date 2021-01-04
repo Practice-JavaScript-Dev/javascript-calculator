b84e6059d7bdc01292250b8a9aecdc7709aa7fa7
jamesnorbertowork <jamesnorbertowork@gmail.com>
2021-01-04 18:19:20 +0900

    power went out ...

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
 /******************************
-MANIPULATE HISTORY RESULT
+GET/PRINT HISTORY FUNCTIONS
 ******************************/
 
-// GET AND RETURN HISTORY VALUE BY ID
-function getHistory(){
+// Return history via the inner text of <p> with the id "history-value"
+function getHistory() {
     return document.getElementById("history-value").innerText;
 }
-// PRINT HISTORY VALUE TO SCREEN
-function printHistory(num){
-    document.getElementById("history-value").innerText = num;
+
+// When called this function replaces whatever is in <p id="history-value"> with "myParameter"
+function printHistory(myParameter) {
+    document.getElementById("history-value").innerText = myParameter;
 }
-// TEMP PRINT TEST
-printHistory("99*1-3")
 
 /******************************
-MANIPULATE OUTPUT RESULT
+GET OUTPUT FUNCTION
 ******************************/
 
-//
+// Return output via the innerText of <p> with the id "output-value"
+function getOutput(){
+    return document.getElementById("output-value").innerText;
+}
+
+/******************************
+PRINT/FORMAT OUTPUT (CONNECTED)
+******************************/
+
+// When called this function replaces whatever's in <p id="output-value"> with "myParameter"
+function printOutput(myParameter) {
+    // We nest a new function call inside --> this enables us to add commas to the value via that functio (addCommasToOutput)
+    document.getElementById("output-value").innerText = addCommasToOutput(myParameter);
+}
+
+// This function contains the logic necessary to add commas (built in methods ....)
+function addCommasToOutput(myParameter) {
+    // Make sure the value is a number
+    var myValue = Number(myParameter);
+    // The toLocaleString() method with "en" automatically adds commas
+    var formattedValue = myValue.toLocaleString("en");
+    // Calling printOuput(myParameter) will return 
+    return formattedValue;
+}
+
+
+// TESTING IT WORKS
+printOutput(1000000)
+
+
+
+/******************************
+IMPLEMENT OPERATIONS
+******************************/
+
+
+
+
